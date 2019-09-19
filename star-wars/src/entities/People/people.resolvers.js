32db9557b32703d509ccc93a6406e7e80fcd1f92
Leonel Peña <leo.dhpl@gmail.com>
2019-09-19 10:42:06 -0300

    feat (star wars): Implement "getPeople" query.

--- star-wars/src/entities/People/people.resolvers.js
+++ star-wars/src/entities/People/people.resolvers.js
@@ -5,6 +5,21 @@ const peopleResolvers = {
     getPerson: (root, args) => {
       return axios.get(`https://swapi.co/api/people/${args.id}`).then(response => response.data);
     },
+    getPeople: (root, args) => {
+      const params = {};
+
+      if (args.page) {
+        params.page = args.page;
+      }
+
+      if (args.search) {
+        params.search = args.search;
+      }
+
+      return axios.get('https://swapi.co/api/people', { params }).then(
+        response => response.data.results
+      );
+    },
   }
 };
 
